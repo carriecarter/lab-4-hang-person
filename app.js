e6aa6e626066326b7e16b0f33ae38a12dfe20b52
carriecarter <carrie@carrie-carter.com>
2018-05-03 14:53:17 -0700

    working on selecting random word from array

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 'use-strict';
 
+
+
+
 // This selects a random word to be guessed by user
 function randomWord(){ 
+    for(i=0 , i>5 , i++) {
+    }
+        var index = getRandomIndex(words.length);
 
-var index = getRandomIndex(words.length);
+        var words = words[index];
 
-var words = words[index];
+        words.splice(index, 1);
 
-words.splice(index, 1);
-
-return words;
-
+        return words;
+    
+        console.log(words);
+       
 }
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
