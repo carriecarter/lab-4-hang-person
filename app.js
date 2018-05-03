e1171ab4942037497fb21b0423ea8a6cbd0f6b7e
Zachar13y <zachar13y@gmail.com>
2018-05-03 14:22:40 -0700

    Added random index for words array.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use-strict';
+
+// This selects a random word to be guessed by user
+function randomWord(){ 
+
+var index = getRandomIndex(words.length);
+
+var words = words[index];
+
+words.splice(index, 1);
+
+return words;
+
+}
