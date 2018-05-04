76065ee917313d6b7f5466da939ee8c48814ae44
Zachar13y <zachar13y@gmail.com>
2018-05-03 17:43:17 -0700

    Split selectedCar into an array

--- app.js
+++ app.js
@@ -6,37 +6,25 @@
 // This selects a random word to be guessed by user
 function loadWord(){ 
     // for(i = 0; i > 5; i++) {
-        var words = ['Honda', 'BMW', 'Tesla', 'Mercedes', 'Audi', 'Porsche'];
+        var listOfWords = ['Honda', 'BMW', 'Tesla', 'Mercedes', 'Audi', 'Porsche'];
         var index = getRandomIndex(listOfWords.length);
         console.log(index);
         
-        var words = listOfWords[index];
-        
+        var car = listOfWords[index];
+        console.log('selected car is ' + car);
         // removes car from array
-        listOfWords.splice(index, 1);
+        // listOfWords.splice(index, 1);
         
         // returns randomly selected car
         console.log(words);
 
-        return words;   
+        var selectedCar = car.split("");
+        console.log(selectedCar);
+
 }
 
 
-// var str = car;
-// var n = str.length;
-// document.getElementById("guess-word").innerHTML = n;
 
-// console.log(n);
-
-
-
-
-// function guess() {
-//     var carString = car.length;
-
-
-
-// }
 
 function getRandomIndex(max) {
     return Math.floor(Math.random() * Math.floor(max));
