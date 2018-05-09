a2fc882c586f67b07418479c746a4be80575cae5
carriecarter <carrie@carrie-carter.com>
2018-05-09 16:07:01 -0700

    tutor time got functionality cleaned up

--- app.js
+++ app.js
@@ -6,9 +6,13 @@
 function getRandomIndex(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
-    
+
+var currentCar = []; 
+
+
 // This selects a random word from the array in words.js to be guessed by user
 function loadWord(){
+        currentCar = [];
         var index = getRandomIndex(words.length);
         carToGuess = words[index];
         console.log('selected car to guess is ' + carToGuess);
@@ -16,43 +20,50 @@ function loadWord(){
         carToGuess = carToGuess.toLowerCase().split("");
         console.log('individual letters of car to guess' , carToGuess);
 
-        // eventually hide the letters on the page and replace with blanks
-        // for now this shows me the letters to guess on screen
-        var p = document.getElementById('car-letters');
-        p.textContent = (carToGuess);
+        for(var i = 0; i < carToGuess.length; i++) {
+            currentCar.push('_');
+        }
+        var p = document.getElementById('blank-spaces');
+        p.textContent = (currentCar);
+
 }
 
+var userGuessLetters = [];
+var counter = 0;
 //guess letter input box and button 
 function guessLetter() {
 
     var userGuess = document.getElementById("guess-box").value;
-    document.getElementById("user-guess").innerHTML = userGuess;
-    console.log('the user guessed the letter ' , userGuess);
+    userGuessLetters.push(userGuess);
+
+    document.getElementById("user-guess").innerHTML = userGuessLetters;
+    console.log('the user guessed the letter ' , userGuessLetters);
         
     if (userGuess === "") {
         alert('Please guess a letter');
     }
 
-    for(var i = 0; i < loadWord.length; i++) {
-        
-        if (userGuess.includes(carToGuess)) {
-            alert ('alert');
-            //document.getElementById("user-guess").innerHTML = 'Good guess';
-        }
+    if (carToGuess.includes(userGuess)) {
+        var indexOfGuess = carToGuess.indexOf(userGuess);
+        console.log(indexOfGuess);
+        currentCar.splice(indexOfGuess, 1, userGuess);
+        counter += 1;    
     }
-                
-    
-        
-    //var p = document.getElementById('correct-guess');
-    //p.textContent = (userGuess);        
+    else {
+        counter += 1;
+    }
 
-    
+    if (counter === carToGuess.length + 5) {
+        alert('WAH WAH WAH WAHHHHHHH');
+    }
+    // inside if statement above- reset counter by re-declaring counter =0 and redeclare empty userGuessLetters
+
+
+    var p = document.getElementById('blank-spaces');
+    p.textContent = (currentCar);
+                       
 }
 
-// Array for correct letters guessed with blanks
 
-var correctLetter = [];
-for(var i = 0; i < loadWord[i]; i++) {
-    correctLetter[i] = '_';
-}
+
 
